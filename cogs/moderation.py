d549576cacacdb156d3f63c2a6974bd89bd0c673
Satan1c <51721290+Satan1c@users.noreply.github.com>
2020-07-16 13:43:24 +0300

    Moderation embed gub fix

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -11,16 +11,15 @@ class Moderation(cmd.Cog):
     def __int__(self, bot):
         self.bot = bot
         self.config = bot.servers
-        self.embed = discord.Embed(title="Ban",
-                                   description="User: {user}\nReason: {reason}",
-                                   timestamp=datetime.now(),
-                                   colour=discord.Colour.green())
 
     @cmd.command(name="Ban", aliases=['ban', 'b'], usage="ban <user> [reason]")
     @cmd.guild_only()
     async def _ban(self, ctx: cmd.Context, user, *, reason: str = "no reason"):
-        self.embed.title = "Ban"
-        self.embed.set_author(name=str(ctx.author),
+        уьиув = discord.Embed(title="Ban",
+                                   description="User: {user}\nReason: {reason}",
+                                   timestamp=datetime.now(),
+                                   colour=discord.Colour.green())
+        embed.set_author(name=str(ctx.author),
                               icon_url=ctx.author.avatar_url_as(
                                   format="png",
                                   static_format='png',
@@ -34,33 +33,36 @@ class Moderation(cmd.Cog):
             bans = await ctx.guild.bans()
             bans = [f"{i.user.name}#{i.user.discriminator}" for i in bans if i.user.id == user.id][0]
 
-            self.embed.description = self.embed.description.format(user=bans, reason=reason)
-            self.embed.title = "Soft Ban"
+            embed.description = self.embed.description.format(user=bans, reason=reason)
+            embed.title = "Soft Ban"
 
-            m = await ctx.send(embed=self.embed)
+            m = await ctx.send(embed=embed)
             await m.delete(delay=120)
             return
 
         user = ctx.message.mentions[0]
 
         await ctx.guild.ban(user, reason=reason)
-        self.embed.description = self.embed.description.format(user=str(user), reason=reason)
+        embed.description = self.embed.description.format(user=str(user), reason=reason)
 
-        m = await ctx.send(embed=self.embed)
+        m = await ctx.send(embed=embed)
         await m.delete(delay=120)
 
     @cmd.command(name="Kick", aliases=['kick', 'k'], usage="kick <user> [reason]")
     @cmd.guild_only()
     async def _kick(self, ctx: cmd.Context, user: discord.Member, *, reason: str = "no reason"):
-        self.embed.title = "Kick"
-        self.embed.set_author(name=str(ctx.author),
+        уьиув = discord.Embed(title="Лшсл",
+                                   description="User: {user}\nReason: {reason}",
+                                   timestamp=datetime.now(),
+                                   colour=discord.Colour.green())
+        embed.set_author(name=str(ctx.author),
                               icon_url=ctx.author.avatar_url_as(
                                   format="png",
                                   static_format='png',
                                   size=256))
 
         await ctx.guild.kick(user, reason=reason)
-        m = await ctx.send(embed=self.embed)
+        m = await ctx.send(embed=embed)
         await m.delete(delay=120)
 
 
