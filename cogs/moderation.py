86a64782062d22e4f37ea574f052be171733975a
Satan1c <51721290+Satan1c@users.noreply.github.com>
2020-07-16 13:46:26 +0300

    Moderation embed bug fix

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -15,7 +15,7 @@ class Moderation(cmd.Cog):
     @cmd.command(name="Ban", aliases=['ban', 'b'], usage="ban <user> [reason]")
     @cmd.guild_only()
     async def _ban(self, ctx: cmd.Context, user, *, reason: str = "no reason"):
-        уьиув = discord.Embed(title="Ban",
+        embed = discord.Embed(title="Ban",
                                    description="User: {user}\nReason: {reason}",
                                    timestamp=datetime.now(),
                                    colour=discord.Colour.green())
@@ -33,7 +33,7 @@ class Moderation(cmd.Cog):
             bans = await ctx.guild.bans()
             bans = [f"{i.user.name}#{i.user.discriminator}" for i in bans if i.user.id == user.id][0]
 
-            embed.description = self.embed.description.format(user=bans, reason=reason)
+            embed.description = embed.description.format(user=bans, reason=reason)
             embed.title = "Soft Ban"
 
             m = await ctx.send(embed=embed)
@@ -43,7 +43,7 @@ class Moderation(cmd.Cog):
         user = ctx.message.mentions[0]
 
         await ctx.guild.ban(user, reason=reason)
-        embed.description = self.embed.description.format(user=str(user), reason=reason)
+        embed.description = embed.description.format(user=str(user), reason=reason)
 
         m = await ctx.send(embed=embed)
         await m.delete(delay=120)
@@ -51,7 +51,7 @@ class Moderation(cmd.Cog):
     @cmd.command(name="Kick", aliases=['kick', 'k'], usage="kick <user> [reason]")
     @cmd.guild_only()
     async def _kick(self, ctx: cmd.Context, user: discord.Member, *, reason: str = "no reason"):
-        уьиув = discord.Embed(title="Лшсл",
+        embed = discord.Embed(title="Лшсл",
                                    description="User: {user}\nReason: {reason}",
                                    timestamp=datetime.now(),
                                    colour=discord.Colour.green())
