52e61ae8bc505fdfbdf116c26998abf358791861
Satan1c <51721290+Satan1c@users.noreply.github.com>
2020-07-25 18:34:07 +0300

    discord boats add

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 SDC = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODU3MDM0MTk3NDczNjkyNiIsImlhdCI6MTU5NTYxMzU0NX0.8hV9VHaZ07W1GPyRo4JX9ujyWSzHvmxgc_yTIIzEMUI"
+Boat = "EJ665CuJlCMEBImkQ3sinuJAfO06Z3zXLagNj1uzvyxEWkZkl8umxV3Oo9BGl3I7EArTTsnbAhGz1x75xU23TEbaeIsRcuP2atjOA0Ov6HTULjktaUKmiJHyW9twVFdj79EzQikTN6iAz8HN4Eoc7p4YVD0"
 
 TOKEN = "NjQ4NTcwMzQxOTc0NzM2OTI2.XwLvPQ.2r9nZktVxcw4FBoLiu_pVd1at-s"
 MONGO = "mongodb+srv://Geno:Atlas23Game@genodb-wrqdw.mongodb.net/test?retryWrites=true&w=majority"
