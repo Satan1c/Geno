44c95fd6b2bec12fc6183f57e824c7aab1d0229f
Alexander <satan1c@ukr.net>
2022-08-09 00:04:01 +0300

    IResult

---
+++ Types/InteractionResult.cs
@@ -0,0 +1,10 @@
+﻿using Discord.Interactions;
+
+namespace Geno.Types;
+
+public class InteractionResult : RuntimeResult
+{
+    public InteractionResult(InteractionCommandError? error, string reason) : base(error, reason)
+    {
+    }
+}
