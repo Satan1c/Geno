1748c10549e9c2d064b727129c9d29e221293afe
Alexander <satan1c@ukr.net>
2022-08-29 16:11:32 +0300

    add UserLocales

---
+++ Errors/UserLocales.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Geno.Errors;
+
+public enum UserLocales
+{
+    [EnumMember(Value = "en-GB")]
+    English = 0,
+    
+    [EnumMember(Value = "da")]
+    Danish
+}
