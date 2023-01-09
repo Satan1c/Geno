c734e021058bd337892f9400744b5c73a77d0f14
Alexander <satan1c@ukr.net>
2023-01-09 13:28:28 +0200

    add permissions static class for checks

---
+++ Utils/BotPermissions.cs
@@ -0,0 +1,8 @@
+﻿using Discord;
+
+namespace Geno.Utils;
+
+public static class BotPermissions
+{
+	public const ChannelPermission UtilsAddVoice = ChannelPermission.ManageChannels;
+}
---
+++ Utils/UserPermissions.cs
@@ -0,0 +1,8 @@
+﻿using Discord;
+
+namespace Geno.Utils;
+
+public static class UserPermissions
+{
+	public const ChannelPermission UtilsAddVoice = ChannelPermission.ManageChannels;
+}
