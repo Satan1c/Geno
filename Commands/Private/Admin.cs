0a61e02b6f8f6b453d71420240050b008984fb37
Alexander <satan1c@ukr.net>
2023-01-18 22:15:53 +0200

    Private admin commands group

---
+++ Commands/Private/Admin.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Interactions;
+using Geno.Utils;
+
+namespace Geno.Commands.Private;
+
+[Group("admin", "admin commands")]
+[Private(Category.Admin)]
+public class Admin : InteractionModuleBase<ShardedInteractionContext>
+{
+	[SlashCommand("reg_category", "slash categories registration")]
+	public async Task Registration(ulong guild, Category category, bool clear = false)
+	{
+		if (CommandHandlingService.Private.TryGetValue(category, out var modules))
+			await CommandHandlingService.Interactions.AddModulesToGuildAsync(guild, clear, modules);
+	}
+}
