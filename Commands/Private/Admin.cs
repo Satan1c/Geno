4dc87b85a1cdb7b34c5a1475057ba359b8fe6eab
Alexander <satan1c@ukr.net>
2023-01-19 11:38:37 +0200

    Update admin category

--- Commands/Private/Admin.cs
+++ Commands/Private/Admin.cs
@@ -1,16 +1,31 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
+using Geno.Responses;
 using Geno.Utils;
 
 namespace Geno.Commands.Private;
 
 [Group("admin", "admin commands")]
 [Private(Category.Admin)]
+[RequireOwner]
 public class Admin : InteractionModuleBase<ShardedInteractionContext>
 {
 	[SlashCommand("reg_category", "slash categories registration")]
+	[RequireOwner]
 	public async Task Registration(ulong guild, Category category, bool clear = false)
 	{
+		await DeferAsync(true);
+		
+		clear = category == Category.None || clear;
+		
 		if (CommandHandlingService.Private.TryGetValue(category, out var modules))
+		{
 			await CommandHandlingService.Interactions.AddModulesToGuildAsync(guild, clear, modules);
+
+			await Context.Respond(new EmbedBuilder().WithColor(Color.Green).WithDescription("Registered"), true, true);
+			return;
+		}
+		
+		await Context.Respond(new EmbedBuilder().WithColor(Color.Red).WithDescription("Category not found"), true, true);
 	}
 }
