cab4a91f9533b02f897f862f6839ef4ef8389e0b
Alexander <satan1c@ukr.net>
2023-02-04 07:04:53 +0200

    Deferred interaction responses fix

--- Responses/Responser.cs
+++ Responses/Responser.cs
@@ -4,24 +4,31 @@ namespace Geno.Responses;
 
 public static class Responser
 {
-	public static Task Respond(this ShardedInteractionContext context,
+	public static Task Respond(this IInteractionContext context,
 		EmbedBuilder embed,
 		bool ephemeral = false,
 		bool isDefered = false)
 	{
-		var em = embed.Build();
+		return context.Respond(embed.Build(), ephemeral, isDefered);
+	}
 
+	public static Task Respond(this IInteractionContext context,
+		Embed embed,
+		bool ephemeral = false,
+		bool isDefered = false)
+	{
 		if (isDefered)
-		{
 			return context.Interaction.ModifyOriginalResponseAsync(x =>
 				{
-					x.Embed = em;
+					var flags = ((MessageFlags)x.Flags!)!;
+					x.Embed = embed;
 					x.AllowedMentions = AllowedMentions.None;
+					x.Flags = ephemeral ? flags ^ MessageFlags.Ephemeral : flags | MessageFlags.Ephemeral;
 				}
 			);
-		}
 
-		return context.Interaction.RespondAsync(embed: em,
+		return context.Interaction.RespondAsync(
+			embed: embed,
 			allowedMentions: AllowedMentions.None,
 			ephemeral: ephemeral
 		);
