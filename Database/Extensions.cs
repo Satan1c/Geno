fc13c77cfc1fb228c3f0fe4a056285f23dfaa1aa
Alexander <satan1c@ukr.net>
2023-04-06 21:11:04 +0300

    specify TDocument as structs

--- Database/Extensions.cs
+++ Database/Extensions.cs
@@ -14,67 +14,27 @@ public static class Extensions
 		ICacheManager<TDocument> cacheManager,
 		FilterDefinition<TDocument> filterDefinition,
 		ulong id)
+		where TDocument : struct
 	{
 		var itemId = id.ToString();
 		if (cacheManager.Exists(itemId))
 			return true;
 
-		var item = await collection.Find(filterDefinition).FirstOrDefaultAsync();
-		if (item == null) return false;
+		var item = await (await collection.FindAsync(filterDefinition)).FirstOrDefaultAsync();
+		if (item.AreSame(default)) return false;
 
 		cacheManager.Put(itemId, item);
 		return true;
 	}
-
-	private static async ValueTask CreateDeletionIndex<TDocument>(this IMongoCollection<TDocument> collection,
-		TimeSpan expirationTime,
-		IndexKeysDefinition<TDocument> indexKeys,
-		string name = "deletion_index")
-	{
-		var indexModel = new CreateIndexModel<TDocument>(indexKeys, new CreateIndexOptions
-		{
-			ExpireAfter = expirationTime,
-			Name = name
-		});
-
-		await collection.Indexes.CreateOneAsync(indexModel);
-	}
-
-	public static async ValueTask RemoveIndex<TDocument>(this IMongoCollection<TDocument> collection,
-		string name = "deletion_index")
-	{
-		await collection.Indexes.DropOneAsync(name);
-	}
-
-	public static async ValueTask ModifyIndex<TDocument>(this IMongoCollection<TDocument> collection,
-		TimeSpan expirationTime,
-		IndexKeysDefinition<TDocument> indexKeys,
-		string name = "deletion_index",
-		string? oldName = null)
-	{
-		await collection.RemoveIndex(oldName ?? name);
-		await collection.CreateDeletionIndex(expirationTime, indexKeys, name);
-	}
-
+	
 	public static async ValueTask InsertOrReplaceOne<TDocument>(this IMongoCollection<TDocument> collection,
 		FilterDefinition<TDocument> filterDefinition,
 		TDocument document)
+		where TDocument : struct
 	{
 		var item = await collection.FindOneAndReplaceAsync(filterDefinition, document);
 
-		if (item == null)
+		if (item.AreSame(default))
 			await collection.InsertOneAsync(document);
-	}
-
-	public static async ValueTask<TDocument> FindOrInsert<TDocument>(this IMongoCollection<TDocument> collection,
-		FilterDefinition<TDocument> filterDefinition,
-		TDocument document)
-	{
-		var item = await collection.Find(filterDefinition).FirstOrDefaultAsync();
-		if (item != null)
-			return item;
-
-		await collection.InsertOneAsync(document);
-		return await collection.Find(filterDefinition).FirstOrDefaultAsync();
 	}
 }
